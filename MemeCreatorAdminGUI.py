05cfa49360487003649e0bef6a386d8d41cf7d03
Michael Fridrikhson <fridrihson.1701@gmail.com>
2018-10-25 21:57:51 +0300

    Set functionality for the text box and added scrollbars for canvas

--- MemeCreatorAdminGUI.py
+++ MemeCreatorAdminGUI.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 from PIL import Image, ImageTk, ImageDraw, ImageFont
-import math
 
 
 class Application:
@@ -15,13 +14,23 @@ class Application:
         self.root.config(menu=self.menubar)
 
     def setup_gui(self):
-        self.canvas = Canvas(self.root, width=self.size[0] - 200, height=self.size[1], bd=2, relief="solid")
-        self.canvas.pack(side=LEFT)
+        self.canvas_frame = Frame(self.root)
+        self.canvas_frame.pack(side=LEFT)
+        self.canvas = Canvas(self.canvas_frame, width=self.size[0] - 200, height=self.size[1], bd=2, relief="solid")
+
         self.canvas.bind("<Button-1>", self.fix_pos)
         self.canvas.bind("<B1-Motion>", self.draw_rect)
         self.canvas.bind("<ButtonRelease-1>", self.draw_final_rect)
-        self.canvas.bind("<Escape>", self.clear_canvas)
-        self.menubar = Menu(root, tearoff=0)
+        self.root.bind("<Escape>", self.clear_canvas)
+        hbar = Scrollbar(self.canvas_frame, orient=HORIZONTAL)
+        hbar.pack(side=BOTTOM, fill=X)
+        hbar.config(command=self.canvas.xview)
+        vbar = Scrollbar(self.canvas_frame, orient=VERTICAL)
+        vbar.pack(side=RIGHT, fill=Y)
+        vbar.config(command=self.canvas.yview)
+        self.canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        self.canvas.pack(expand=True,fill=BOTH)
+        self.menubar = Menu(self.root, tearoff=0)
         filemenu = Menu(self.menubar, tearoff=0)
         filemenu.add_command(label="Open Image", command=self.open_image)
         filemenu.add_command(label="Close Image", command=self.close_image)
@@ -45,19 +54,22 @@ class Application:
             self.rotation.pack()
             self.entry_label = Label(self.toolbar, text="Test with text input")
             self.entry_label.pack()
-            self.text_entry = Entry(self.toolbar)
+            self.sv = StringVar()
+            self.sv.trace_add("write", self.get_text)
+            self.text_entry = Entry(self.toolbar, textvariable=self.sv)
             self.text_entry.pack()
-        if self.img.size[0] > self.size[0]:
+        '''if self.img.size[0] > self.size[0]:
             wpercent = ((self.size[0] - 200) / float(self.img.size[0]))
             hsize = int((float(self.img.size[1]) * float(wpercent)))
             self.img = self.img.resize((self.size[0] - 200, hsize), Image.ANTIALIAS)
         if self.img.size[1] > self.size[1]:
             hpercent = ((self.size[1]) / float(self.img.size[1]))
             wsize = int((float(self.img.size[0] - 200) * float(hpercent)))
-            self.img = self.img.resize((wsize, self.size[1]), Image.ANTIALIAS)
+            self.img = self.img.resize((wsize, self.size[1]), Image.ANTIALIAS)'''
         self.meme = ImageTk.PhotoImage(self.img)
         self.canvas.delete(ALL)
         self.canvas.create_image((self.size[0] - 200)/2.0, self.size[1]/2.0, anchor=CENTER, image=self.meme)
+        self.canvas.config(scrollregion = self.canvas.bbox("all"))
 
 
     def close_image(self):
@@ -84,6 +96,11 @@ class Application:
 
     def draw_final_rect(self, event):
         self.x2, self.y2 = event.x, event.y
+
+    def fix_pos(self, event):
+        self.x1, self.y1 = event.x, event.y
+
+    def get_text(self, *args):
         if self.x2 < self.x1:
             self.x1, self.x2 = self.x2, self.x1
         if self.y2 < self.y1:
@@ -96,7 +113,7 @@ class Application:
             font_type = ImageFont.truetype("../MemeCreatorBot/impact.ttf", font_size)
             im = Image.new("RGBA", (width, height), (255, 255, 255, 0))
             draw = ImageDraw.Draw(im)
-            text = "testing text testing text testing text"
+            text = self.sv.get()
             modifiedtext = ''
             currstr = ''
             if draw.textsize(text, font=font_type)[0] < width:
@@ -126,14 +143,9 @@ class Application:
             W, H = draw.textsize(modifiedtext, font=font_type)
             draw.text(((width - W) / 2.0, (height - H) / 2.0), modifiedtext, fill=(0, 0, 0),
                       font=font_type, spacing=3, align='center')
-            im.show()
-
             self.text_img = ImageTk.PhotoImage(im)
             self.canvas.create_image(self.x1, self.y1, anchor=NW, image=self.text_img)
             del im
-
-    def fix_pos(self, event):
-        self.x1, self.y1 = event.x, event.y
 
 
 root = Tk()
