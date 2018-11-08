be595b42c482920f7ece7a97e88f553461a15247
Michael Fridrikhson <fridrihson.1701@gmail.com>
2018-11-08 23:03:44 +0200

    Added functionality for listbox, buttons; implemented transforming data into the strings

--- MemeCreatorAdminGUI.py
+++ MemeCreatorAdminGUI.py
@@ -12,7 +12,9 @@ class Application:
         self.x1, self.x2, self.y2, self.y1 = 0, 0, 0, 0
         self.text_color = "(0, 0, 0)"
         self.line1 = 0
-        self.areas_info = []
+        self.areas_info = dict()
+        self.class_data = ""
+        self.dict_data = ""
         self.root.config(menu=self.menubar)
 
     def setup_gui(self):
@@ -68,14 +70,16 @@ class Application:
             meme_var_name_input = Entry(self.toolbar, textvariable=self.meme_var_name)
             meme_var_name_input.grid(row=1, column=1)
             add_area_button = Button(self.toolbar, text="Add content area")
+            add_area_button.bind("<Button-1>", self.add_area)
             add_area_button.grid(row=2, column=0, sticky=S)
             delete_area_button = Button(self.toolbar, text="Delete content area")
+            delete_area_button.bind("<Button-1>", self.delete_area)
             delete_area_button.grid(row=3, column=0)
             save_area_button = Button(self.toolbar, text="Save current meme")
-            save_area_button.bind("<Button-1>")
+            save_area_button.bind("<Button-1>", self.save_area)
             save_area_button.grid(row=4, column=0, sticky=N)
-            datalist = Listbox(self.toolbar)
-            datalist.grid(row=2, column=1, rowspan=3, pady=(20, 0))
+            self.datalist = Listbox(self.toolbar, selectmode=EXTENDED)
+            self.datalist.grid(row=2, column=1, rowspan=3, pady=(20, 0))
             rotate_label = Label(self.toolbar, text="Set the rotation of the area:")
             rotate_label.grid(row=5, columnspan=2, pady=10)
             rotation = Scale(self.toolbar, from_=-180, to=180, tickinterval=180, orient=HORIZONTAL, length=180)
@@ -101,6 +105,45 @@ class Application:
         self.canvas.delete(ALL)
         self.canvas.create_image(img.width / 2.0, img.height / 2.0, anchor=CENTER, image=self.meme)
         self.canvas.config(scrollregion=self.canvas.bbox("all"))
+
+    def add_area(self, event):
+        if self.x1 > 0 and self.x2 > 0 and self.y1 > 0 and self.y2 > 0:
+            self.areas_info[(self.width, self.height)] = (self.x1, self.y1)
+            self.datalist.insert(END, "(" + str(self.width) + ", " + str(
+                self.height) + ")" + ": " + "(" + str(self.x1) + ", " + str(self.y1) + ")")
+            print(self.areas_info)
+        else:
+            messagebox.showwarning("No area was selected", "Please select an area on the picture")
+
+    def delete_area(self, event):
+        if self.datalist.curselection():
+            selection = self.datalist.curselection()
+            for i in selection[::-1]:
+                width = int(self.datalist.get(i).split(":")[0].split(", ")[0][1:])
+                height = int(self.datalist.get(i).split(":")[0].split(", ")[1][:-1])
+                del self.areas_info[(width, height)]
+                print(self.areas_info)
+                self.datalist.delete(i)
+        else:
+            messagebox.showwarning("Select area to delete", "Please, select area or areas in the list to delete")
+
+    def save_area(self, event):
+        if self.datalist.index(END) != 0 and self.meme_var_name.get() != "" and self.meme_name:
+            self.class_data += (self.meme_var_name.get().strip() + " = Meme({")
+            counter = 1
+            for w, h in self.areas_info:
+                self.class_data += ("(" + str(w) + ", " + str(h) + "): (" + str(self.areas_info[w, h][0]) + ", " + str(self.areas_info[w, h][1]) + ")")
+                counter += 1
+                if counter <= len(self.areas_info):
+                    self.class_data += ", "
+            self.class_data += "}, 'impact.ttf', "
+            self.class_data += self.text_color
+            self.class_data += ")"
+            # print(self.class_data)
+            self.dict_data += (", '" + self.meme_name.get().strip().lower() + "': " + self.meme_var_name.get().strip())
+            # print(self.dict_data)
+        else:
+            messagebox.showwarning("No data to save", "Please input all the necessary data")
 
     def close_image(self):
         if self.canvas.find_all():
@@ -135,41 +178,41 @@ class Application:
     def draw_final_rect(self, event):
         canvas = event.widget
         self.x2, self.y2 = canvas.canvasx(event.x), canvas.canvasy(event.y)
-
-    def fix_pos(self, event):
-        canvas = event.widget
-        self.x1, self.y1 = canvas.canvasx(event.x), canvas.canvasy(event.y)
-
-    def get_text(self, *args):
         if self.x2 < self.x1:
             self.x1, self.x2 = self.x2, self.x1
         if self.y2 < self.y1:
             self.y1, self.y2 = self.y2, self.y1
-        width, height = int((self.x2 - self.x1)), int((self.y2 - self.y1))
-        if width < 30 or height < 30:
+        self.width, self.height = int((self.x2 - self.x1)), int((self.y2 - self.y1))
+
+    def fix_pos(self, event):
+        canvas = event.widget
+        self.x1, self.y1 = int(canvas.canvasx(event.x)), int(canvas.canvasy(event.y))
+
+    def get_text(self, *args):
+        if self.width < 30 or self.height < 30:
             messagebox.showwarning("Small content area", "Content area is too small, please select larger area")
         else:
             font_size = 40
             font_type = ImageFont.truetype("../MemeCreatorBot/impact.ttf", font_size)
-            im = Image.new("RGBA", (width, height), (255, 255, 255, 0))
+            im = Image.new("RGBA", (self.width, self.height), (255, 255, 255, 0))
             draw = ImageDraw.Draw(im)
             text = self.sv.get()
             modifiedtext = ''
             currstr = ''
-            if draw.textsize(text, font=font_type)[0] < width:
+            if draw.textsize(text, font=font_type)[0] < self.width:
                 modifiedtext = text
             else:
                 words = text.split()
                 for i in range(len(words)):
-                    if draw.textsize(currstr, font=font_type)[0] + draw.textsize(words[i], font=font_type)[0] < width:
+                    if draw.textsize(currstr, font=font_type)[0] + draw.textsize(words[i], font=font_type)[0] < self.width:
                         currstr += words[i] + ' '
                     else:
                         modifiedtext += currstr + '\n'
                         currstr = words[i] + ' '
                     if i == len(words) - 1:
                         modifiedtext += currstr
-                while draw.textsize(modifiedtext, font=font_type)[1] > height or \
-                        draw.textsize(modifiedtext, font=font_type)[0] > width:
+                while draw.textsize(modifiedtext, font=font_type)[1] > self.height or \
+                        draw.textsize(modifiedtext, font=font_type)[0] > self.width:
                     font_size -= 1
                     font_type = ImageFont.truetype("../MemeCreatorBot/impact.ttf", font_size)
             datas = im.getdata()
@@ -181,7 +224,7 @@ class Application:
                     new_data.append(item)
             im.putdata(new_data)
             W, H = draw.textsize(modifiedtext, font=font_type)
-            draw.text(((width - W) / 2.0, (height - H) / 2.0), modifiedtext, fill=(0, 0, 0),
+            draw.text(((self.width - W) / 2.0, (self.height - H) / 2.0), modifiedtext, fill=(0, 0, 0),
                       font=font_type, spacing=3, align='center')
             self.text_img = ImageTk.PhotoImage(im)
             self.canvas.create_image(self.x1, self.y1, anchor=NW, image=self.text_img)
